66fd1aab605f74dbaa1d1990fcf0d6f0346f66c8
Lyric <5h3ll3x@gmail.com>
2023-10-22 19:41:12 +0900

    build demo

--- src/index.ts
+++ src/index.ts
@@ -22,14 +22,6 @@ const QuailUI = {
     }
 
     document.body.addEventListener('click', (e) => {
-      // hide popup menu
-      // let trigger = parseInt(sessionStorage.getItem('quailui_global_popup_trigger') || '0') || 0;
-      // sessionStorage.setItem('quailui_global_popup_trigger', (trigger + 1) + '');
-      // window.dispatchEvent(new StorageEvent("storage", {
-      //   key: 'quailui_global_popup_trigger',
-      //   newValue: (trigger + 1) + '',
-      //   oldValue: trigger + ''
-      // }));
       closePopupMenu();
     })
 
--- vite.config.ts
+++ vite.config.ts
@@ -5,42 +5,50 @@ import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    scss({
-      fileName: 'quail-ui.css',
-      include: [
-        "styles/base.scss",
-        "styles/component.scss",
-      ],
-    }),
-  ],
-  build: {
-    sourcemap: true,
-    lib: {
+export default defineConfig(({ mode }) => {
+  const ret = {
+    plugins: [
+      vue(),
+      scss({
+        fileName: 'quail-ui.css',
+        include: [
+          "styles/base.scss",
+          "styles/component.scss",
+        ],
+      }),
+    ],
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        // make sure to externalize deps that shouldn't be bundled
+        // into your library
+        external: ['vue'],
+        output: {
+          // Provide global variables to use in the UMD build
+          // for externalized deps
+          globals: {
+            vue: 'Vue',
+          },
+        },
+      },
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    }
+  }
+  if (mode === 'lib') {
+    ret.build['lib'] = {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'QuailUI',
       // the proper extensions will be added
       fileName: 'index',
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['vue'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  } else if (mode === 'demo') {
+    //
   }
+
+  return ret;
 })
